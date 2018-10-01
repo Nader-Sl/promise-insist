94592f702bc3af2e421a9d2481aea24649f52253
Nader <nader.sleiman@gmail.com>
2018-10-01 14:49:59 +0300

    Initial Commit

---
+++ src/example.ts
@@ -0,0 +1,28 @@
+import PromiseManager from '.'
+
+function getRand(min, max): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function exponentialDelay(maxRetries: number, retryNumber: number) {
+  const baseDelay = 1000;
+  const delay = 2 ** (maxRetries - retryNumber) * 100;
+  const randomSum = delay * 0.2 * Math.random();
+  return baseDelay + delay + randomSum;
+}
+
+const getMagicRandRetriever = () => new Promise<number>((resolve, reject) => {
+  const magicNumber = getRand(1, 10)
+  if (magicNumber === 5) resolve(magicNumber)
+  else reject('Not the random magic number.')
+});
+
+const promiseManager = new PromiseManager(30, exponentialDelay);
+const t1_ID = 't1'
+const t2_ID = 't2'
+promiseManager.retry<number>(t1_ID,getMagicRandRetriever).then(res => { console.log(`t1 : Magic number ${res} was guessed!`) }).catch(err => console.log(`${t1_ID}: ${err}`))
+promiseManager.retry<number>(t2_ID,getMagicRandRetriever).then(res => { console.log(`t2 : Magic number ${res} was guessed!`) }).catch(err => console.log(`${t2_ID} : ${err}`))
+setTimeout(() => promiseManager.cancel(t1_ID), 5000)
+setTimeout(() => promiseManager.cancel(t2_ID), 8000)
---
+++ src/lib/promise-manager.ts
@@ -0,0 +1,95 @@
+export type DelayFunc = ((maxRetries: number, retries: number) => number)
+export type PromiseRetriever<T> = () => Promise<T>;
+export type ID = number | string
+export type ErrorWhitelist = (error: any) => boolean;
+
+export type Config = {
+  retries: number;
+  delay: number | DelayFunc;
+  errorWhitelist: ErrorWhitelist;
+}
+
+interface MetaData {
+  canceled?: boolean;
+  timeout?: number;
+  starttime?: number;
+  resolve?: any;
+}
+
+export default class PromiseManager {
+
+  private retries: number = 10;
+  private delay: number | DelayFunc = 1000;
+  private taskMeta: Map<number | string, MetaData> = new Map();
+  private errorWhitelist: ErrorWhitelist = err => true;
+  public log: boolean = true;
+
+
+  /**
+   * 
+   * @param retries Number of retries, default is 10
+   * @param delay the delay in ms as a Number or DelayFunc, Default is 1000
+   * @param errorWhitelist a function that allows retrying only the whitelisted error. 
+   */
+
+  constructor(retries?: number, delay?: number | DelayFunc, errorWhitelist?: ErrorWhitelist) {
+    if (retries !== undefined) this.retries = retries;
+    if (delay !== undefined) this.delay = delay;
+    if (errorWhitelist !== undefined) this.errorWhitelist = errorWhitelist;
+  }
+
+  /**
+   * 
+   * @param id the id associated with the retryable promise/task
+   */
+
+  public async cancel(id) {
+    if (!this.taskMeta.has(id)) return false;
+    const meta = this.taskMeta.get(id)
+    if (meta !== undefined) {
+      this.taskMeta.set(id, { ...meta, canceled: true })
+      if ('timeout' in meta) {
+        clearTimeout(meta.timeout)
+        meta.resolve()
+        return true;
+      }
+    }
+  }
+
+  /**
+   * 
+   * @param id ID of the promise/task
+   * @param promiseRetriever A function that when executed returns a promise
+   * @param config Optional configuration , if not specified the config passed in the constructor will be used, if that latter wasn't specified either, the default will be used .
+   */
+
+  public async retry<T>(id: ID, promiseRetriever: PromiseRetriever<T>, config: Config = { retries: this.retries, delay: this.delay, errorWhitelist: this.errorWhitelist }): Promise<T> {
+    if (this.taskMeta.has(id)) throw new Error('Promise already pending')
+    this.taskMeta.set(id, { canceled: false, starttime: Date.now() })
+    return this._retry<T>(id, promiseRetriever, config, config.retries)
+  }
+
+  private async _retry<T>(id: ID, promiseRetriever: PromiseRetriever<T>, config: Config, maxRetries: number): Promise<T> {
+    try {
+      const result = await promiseRetriever();
+      this.taskMeta.delete(id)
+      return result;
+    } catch (err) {
+      const metaData = this.taskMeta.get(id) as MetaData;
+      delete metaData.timeout
+      if (!config.errorWhitelist(err) || config.retries === 1 || metaData.canceled) {
+        if (this.log && metaData.canceled) console.log(`Canceled task of ID : ${id} (~ ${Date.now() - (metaData.starttime || 0)} ms)`)
+        this.taskMeta.delete(id)
+        throw new Error(err)
+      }
+
+      let delay = config.delay;
+      if (typeof delay === 'function') {
+        delay = delay(maxRetries, config.retries)
+      }
+      if (this.log) console.log(`Retrying ${id} after ${delay} ms`)
+      config.retries -= 1
+      return new Promise<T>(resolve => metaData!.timeout = setTimeout(metaData!.resolve = () => resolve(this._retry<T>(id, promiseRetriever, config, maxRetries)), delay as number))
+    }
+  }
+}
