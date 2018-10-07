3b74401e488437a7da89dc15bbeb7942ea18bb81
Nader <nader.sleiman@gmail.com>
2018-10-07 23:40:46 +0300

    updated docs

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var typedocs = require("gulp-typedoc");
 var gulp = require("gulp")
 gulp.task("default", function() {
     return gulp
-        .src(["src/lib/*.ts","src/presets/*.ts"])
+        .src(["lib/*.ts","presets/*.ts"])
         .pipe(typedocs({
             
             module: "commonjs",
